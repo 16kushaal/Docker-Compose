52981b3e836140956e65408837b0d83782d75f86
16kushaal <kushaal.1607@gmail.com>
2025-05-07 12:27:49 +0530

    Initial

--- service1/app.py
+++ service1/app.py
@@ -1,14 +1,11 @@
 from flask import Flask
 import requests
-app = Flask(__name__)
+
+app =Flask(__name__)
+
 @app.route('/')
 def hello():
-    try:
-        # Make a request to Service 2
-        response = requests.get('http://service2:5001')
-        return f"Service 2 get response: {response.text}"
-    except Exception as e:
-        return f"Failed to connect to Service 2: {e}"
+    return "Hello from Service 1!"
 
-if __name__ == '_main_':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__=="main_":
+        app.run(host='0.0.0.0', port =5000, debug=True)
