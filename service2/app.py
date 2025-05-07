b87cf2afaca319d33d39dd323e7154ac0da9c423
16kushaal <kushaal.1607@gmail.com>
2025-05-07 12:39:00 +0530

    Working

--- service2/app.py
+++ service2/app.py
@@ -12,4 +12,4 @@ def hello():
         return f"Failed to connect to service1: {e}"
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5001, debug=True)
